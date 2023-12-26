8f7fddd5228b54ecfe2137e76db3e2eaea8417c7
HaHaHaHugo <Navapol_naksen@cmu.ac.th>
2023-12-26 15:23:09 +0700

    lab13_1.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int a);
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+int fibonacci(int a){
+    if(a<=1) return a;
+    else return fibonacci(a-1)+fibonacci(a-2);
 }
